347da9a56d3167c8d7d534817c46892e4391596a
Olivier Fortier <olivier_fortier@outlook.com>
2021-01-14 18:38:17 -0500

    mise a jour de l'app

---
+++ src/components/Intro.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>allo</p>
+    <h1 class="fond-noir" id="yeet">bonsoir</h1>
+    <span> {{ allo }} </span>
+    <span> {{ compteur }}</span>
+  </div>
+</template>
+
+<style scoped>
+.fond-noir {
+  background-color: slategray;
+  color: whitesmoke;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      allo: "coucou",
+      compteur: 0,
+    };
+  },
+};
+</script>
---
+++ src/components/MonArticle.vue
@@ -0,0 +1,26 @@
+<template>
+   <article>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores,
+        distinctio doloribus iusto provident voluptas consequuntur assumenda
+        repellendus optio tenetur quae sit porro praesentium molestiae? Labore,
+        ad cumque! Corrupti, sapiente ullam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores,
+        distinctio doloribus iusto provident voluptas consequuntur assumenda
+        repellendus optio tenetur quae sit porro praesentium molestiae? Labore,
+        ad cumque! Corrupti, sapiente ullam.
+      </p>
+    </article>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
